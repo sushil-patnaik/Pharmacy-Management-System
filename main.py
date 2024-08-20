79b73a36698d79b656004b8cf9b8536569996668
Sushil Patnaik <sp.sushilpatnaik@gmail.com>
2024-08-21 02:05:05 +0530

    Test Push

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import streamlit
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
+import streamlit as st
+# import pandas as pd
+# import numpy as np
+# from matplotlib import pyplot as plt
 
 
+st.set_page_config(page_title="Pharmacy Management System")
+
+st.title("Pharmacy Management System")
